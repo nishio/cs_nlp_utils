23841f6951a3e7e7468d82b98d9b69dc8877bd97
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2014-03-07 14:56:24 +0900

    support to make userdic

---
+++ make_userdic.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8
+"""
+mecabのユーザ辞書を作る
+"""
+output = 'words.csv'
+data = """
+自然数, 標準偏差, テイラー展開, インターフェイス,
+X11, P2P, 7zip, SHA1, C#, C++, Tcl/Tk, Objective C,
+.NET, 条件分岐, インターフェイス,
+"""
+
+fo = file(output, 'w')
+words = [x.strip() for x in data.split(',')]
+
+for w in words:
+    if not w: continue
+    fo.write('%s,,,500,名詞,一般,*,*,*,*,*\n' % w)
+
+fo.close()
